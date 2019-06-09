1462bf9df6b64a550203f1dc5513d5a48712215b
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2019-06-09 12:53:56 +0200

    Fix swagger issue by the resource handler

--- src/main/java/com/bfwg/config/WebConfig.java
+++ src/main/java/com/bfwg/config/WebConfig.java
@@ -13,6 +13,12 @@ public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
     }
