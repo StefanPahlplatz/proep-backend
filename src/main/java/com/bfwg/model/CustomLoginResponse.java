cf75350cbbe1c92f9817e11a23deb5e6fe0f2d7a
Miroslav Zahariev <m.zahariev@student.fontys.nl>
2019-06-11 19:58:24 +0200

    Added getters and setters

--- src/main/java/com/bfwg/model/CustomLoginResponse.java
+++ src/main/java/com/bfwg/model/CustomLoginResponse.java
@@ -6,6 +6,22 @@ public class CustomLoginResponse {
 
     private UserTokenState userToken;
 
+    public User getLoggedInUser() {
+        return loggedInUser;
+    }
+
+    public UserTokenState getUserToken() {
+        return userToken;
+    }
+
+    public void setLoggedInUser(User loggedInUser) {
+        this.loggedInUser = loggedInUser;
+    }
+
+    public void setUserToken(UserTokenState userToken) {
+        this.userToken = userToken;
+    }
+
     public CustomLoginResponse(User user, UserTokenState token){
         this.loggedInUser = user;
         this.userToken = token;
