cf627a0e0d5b36afb96a8e58e69d47fd36855a06
Stefan Pahlplatz <stefanpahlplatz@inogo.nl>
2019-06-12 20:41:37 +0200

    Fake images (#68)
    * Improved fake script
    
    * Update images

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,18 +29,32 @@ INSERT INTO user_authority (user_id, authority_id) VALUES (1, 2);
 INSERT INTO user_authority (user_id, authority_id) VALUES (2, 2);
 INSERT INTO user_authority (user_id, authority_id) VALUES (3, 2);
 
-INSERT INTO vehicle (registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
-  VALUES('SYEB233','red',23424,'X5','BMW','SUV',200,51.441643,5.469722,1);
+INSERT INTO vehicle (id,registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
+  VALUES(1,'SYEB233','red',23424,'X5','BMW','SUV',200,51.441643,5.469722,1);
 
-INSERT INTO vehicle (registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
-  VALUES('S34E33','black',0,'RX8','MAZDA','COUPE',100,51.443343,5.169722,2);
+INSERT INTO image (id,path,vehicle)
+  VALUES(1, 'https://pictures.dealer.com/b/bmwofhonoluluhi/0435/191f0a42830392868afc89f738a8ac92x.jpg?impolicy=resize&w=650', 1);
 
-INSERT INTO vehicle (registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
-  VALUES('SSUHFD3','red',50000,'S2','MERCEDES','SUV',150,48.856613,2.352222,2);
+INSERT INTO vehicle (id,registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
+  VALUES(2,'S34E33','black',0,'RX8','MAZDA','COUPE',100,51.443343,5.169722,2);
 
-INSERT INTO vehicle (registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
-  VALUES('UHSFDF3','blue',60000,'E-TYPE','JAGUAR','SPORT',400,51.507351,-0.127758,3);
+INSERT INTO image (id,path,vehicle)
+  VALUES(2, 'https://upload.wikimedia.org/wikipedia/commons/5/57/2006_Mazda_RX-8_2.6.jpg', 2);
 
-INSERT INTO vehicle (registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
-  VALUES('HDUSDD','white',20000,'AURIS','TOYOTA','HATCHBACK',50,52.370216,4.895168,4);
+INSERT INTO vehicle (id,registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
+  VALUES(3,'SSUHFD3','red',50000,'S2','MERCEDES','SUV',150,48.856613,2.352222,2);
 
+INSERT INTO image (id,path,vehicle)
+  VALUES(3, 'https://www.s2-cc.com/wp-content/uploads/2016/11/dsc03512-1024x684.jpg', 3);
+
+INSERT INTO vehicle (id,registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
+  VALUES(4,'UHSFDF3','blue',60000,'E-TYPE','JAGUAR','SPORT',400,51.507351,-0.127758,3);
+
+INSERT INTO image (id,path,vehicle)
+  VALUES(4, 'https://scd.france24.com/en/files/imagecache/rfi_16x9_1024_578/article/image/jaguar-e-type-electric-2.jpg', 4);
+
+INSERT INTO vehicle (id,registration,colour,mileage,model,make,type,price,longitude,latitude,user_id)
+  VALUES(5,'HDUSDD','white',20000,'AURIS','TOYOTA','HATCHBACK',50,52.370216,4.895168,4);
+
+INSERT INTO image (id,path,vehicle)
+  VALUES(5, 'https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/toyota-auris-side-profile.jpg?itok=WTuCsY_y', 5);
